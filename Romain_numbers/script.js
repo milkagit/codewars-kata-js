7fb41f4289900bd184039399bff89ffe362c8fd6
Milka Deneva <milka.deneva@raketadesign.com>
2021-08-10 19:58:15 +0300

    Roman numbers

---
+++ Romain_numbers/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function solution(number) {
+  let result = "",
+    decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+    roman = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I",
+    ];
+
+  decimals.map(function (value, index) {
+    while (number >= value) {
+      result += roman[index];
+      number -= value;
+    }
+  });
+  return result;
+}
+
+console.log(solution(4));
